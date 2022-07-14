1c0218a80bf42498dfd91a9a87822c21f3822306
shubham kumar <devshubhamyadav@gmail.com>
2022-07-14 16:28:04 +0530

    product fix

---
+++ controllers/product/get-product-byId.js
@@ -0,0 +1,25 @@
+import Product from "../../models/Product.js";
+import { validationErrorHandler } from "../../helpers/validation-error-handler.js";
+
+export const getProductById = async (req, res, next) => {
+  validationErrorHandler(req, next);
+  try {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    if (!product) {
+      const error = new Error("Product Details not found");
+      error.statusCode = 404;
+      return next(error);
+    } else {
+      res.status(200).json({
+        success: true,
+        product,
+      });
+    }
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,10 +4,12 @@ import express from "express";
 import { getAllProducts } from "../controllers/product/get-all-products.js";
 import { getProductDetails } from "../controllers/product/get-product-details.js";
 import { SearchProducts } from "../controllers/product/SearchProducts.js";
+import { getProductById } from "../controllers/product/get-product-byId.js";
 const router = express.Router();
 
 router.get("/get-all-products", getAllProducts);
 router.get("/get-product-details/:slug", getProductDetails);
+router.get("/get-product-byid/:id", getProductById);
 router.get("/search-products", SearchProducts);
 
 export default router;
