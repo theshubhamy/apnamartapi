8387c562c4ebcf33cd388e1e3cd661f75acd2157
Shubham Kumar <devshubhamyadav@gmail.com>
2025-04-03 01:04:38 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 mongoose.set('strictQuery', false);
 const conn = mongoose.connect(
-  "mongodb+srv://adminapnamart:pswdapnamart@ecommerceapi.2w1dbcy.mongodb.net/apnamartdb"
+  "mongodb+srv://test:test@mojfnkf.2w1dbcy.mongodb.net/apnamartdb"
 );
 export default conn;
